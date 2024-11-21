99bb0539001c4407c35ae8e18e6a4728c964e4f5
gustavo_prehl <gustavo_prehl@hotmail.com>
2024-11-21 13:48:24 -0300

    Second class

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 import express from "express";
 
+const posts = [
+    {
+      id: 1,
+      description: "Gato 1",
+      img: "https://placecats.com/millie/300/158"
+    },
+    {
+      id: 2,
+      description: "Gato 2",
+      img: "https://placecats.com/millie/300/158"
+    },
+    {
+      id: 3,
+      description: "Gato 3",
+      img: "https://placecats.com/millie/300/158"
+    },
+];
+
 const app = express();
+app.use(express.json);
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log("Listening server");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function searchPostId(id){
+    return posts.findIndex((post) => {
+      return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = searchPostId(req.params.id);
+  res.status(200).json(posts[index]);
 });
